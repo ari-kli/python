2ac53847ed72299a8f87f759c0e32b11d16cdb63
AK <ariklick@gmail.com>
2023-12-30 07:32:55 -0500

    Add files via upload

---
+++ python_lightbox_print_table.py
@@ -0,0 +1,17 @@
+html_input = '<img src="/guide-media/01HB2XG9F28D5C608C40PXJNNX" alt="Keep-Only-Part-of-Clip-transcript-02.png">'
+
+split_html = html_input.split("alt")
+
+print(split_html)
+
+
+default_url = "/URL"
+default_alt = "FILENAME"
+
+html_code = f'<a href="{default_url}" data-fancybox="lightbox" data-caption="">\n'
+html_code += f'<img src="{default_url}" alt="{default_alt}" />\n'
+html_code += '</a>'
+
+print(html_code)
+
+print(html_input)
