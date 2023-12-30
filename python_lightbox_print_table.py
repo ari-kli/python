b33b80283db15b3a2369b71e36164bfc8e208473
AK <ariklick@gmail.com>
2023-12-30 12:53:15 +0000

    initial commit

--- python_lightbox_print_table.py
+++ python_lightbox_print_table.py
@@ -11,7 +11,7 @@ default_alt = "FILENAME"
 html_code = f'<a href="{default_url}" data-fancybox="lightbox" data-caption="">\n'
 html_code += f'<img src="{default_url}" alt="{default_alt}" />\n'
 html_code += '</a>'
-
+# comment here
 print(html_code)
 
 print(html_input)
